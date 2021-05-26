e177225d86b81170482ae7db5c130582bc970b87
Meijden <m.h.m.v.d.meijden@student.tue.nl>
2021-05-26 11:48:00 +0200

    Prioritized replay buffer implemented plus a different loss function

--- Main/MAIN - Mike.py
+++ Main/MAIN - Mike.py
@@ -10,10 +10,10 @@ import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import binary_crossentropy
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.initializers import RandomNormal
 
 from collections import deque
-import random
 
 # Load environment
 env = or_gym.make('Knapsack-v2')
@@ -22,20 +22,31 @@ env.mask = False
 print("Action Space: {}".format(env.action_space))
 print("State space: {}".format(env.observation_space))
 
-# An episode a full game
-train_episodes = 5_000
-test_episodes = 100
-
+#___ PARAMETERS ___#
+TRAIN_EPSISODES = 10_000
+TEST_EPSISODES = 100
+LEARNING_RATE = 0.4 
+DISCOUNT_RATE = 0.9 
+MIN_REPLAY_SIZE = 250
+BATCH_SIZE = 100 # Number of items to be sampled
+FACTOR = 2 # How much one wants to punish or prize high rewards
+HIGH_PER = 10 # The high and low percentage of rewards in the replay memory
+MAX_EPSILON = 1 # You can't explore more than 100% of the time
+MIN_EPSILON = 0.01 # At a minimum, we'll always explore 1% of the time
+DECAY = 0.0005 # How fast the epsilon decays
+#_________________#
 
 def agent(item_count = env.N):
     """ 
     Requires the values of all the available items
     """
+    init = RandomNormal(mean=0.0, stddev=0.05, seed=10)
     model = Sequential()
-    model.add(Dense(item_count * 2, activation="relu", input_shape=(item_count,)))
-    model.add(Dense(item_count * 2, activation="relu"))
-    model.add(Dense(item_count, activation="sigmoid"))
-    model.compile(Adam(1e-3), binary_crossentropy, metrics=["accuracy"])
+    model.add(Dense(item_count * 3, activation="relu", input_shape=(item_count,), kernel_initializer = init))
+    model.add(Dense(item_count * 2, activation="relu", kernel_initializer = init))
+    model.add(Dense(item_count, activation="relu", kernel_initializer = init))
+    model.add(Dense(item_count, kernel_initializer = init))
+    model.compile(Adam(1e-3), MeanSquaredError(), metrics=["accuracy"])
     
     return model
 
@@ -43,39 +54,39 @@ def train(env, replay_memory, model, target_model, done):
     """
     Train model with Belman equation
     """
-    learning_rate = 0.2 # Learning rate
-    discount_factor = 0.8 #0.618
-
-    MIN_REPLAY_SIZE = 1000
-    if len(replay_memory) < MIN_REPLAY_SIZE:
+    len_replay = len(replay_memory)
+    if len_replay < MIN_REPLAY_SIZE:
         return
 
-    batch_size = 50 * 2
-    mini_batch = random.sample(replay_memory, batch_size)
-    #print(mini_batch)
-    #print("Len minibatch", len(mini_batch))
-    current_states = np.array([transition[0] for transition in mini_batch])
-    #print(current_states.shape)
-    #print(current_states)
-    current_qs_list = model.predict(current_states)
-    new_current_states = np.array([transition[3] for transition in mini_batch])
+    rewards = [transition[2] for transition in replay_memory] # Extract rewards from replay memory
+    rewards.sort() # Sort the rewards list  
+    nr_items = int(len_replay / HIGH_PER) # Number of items in the 'HIGH_PER' range
+    high_list = [ x * (((i * FACTOR) + nr_items) / nr_items) for x, i in zip(rewards[-nr_items:], range(nr_items))] # Apply factor to high rewards 
+    low_list = [ x * (i / (nr_items * FACTOR)) for x, i in zip(rewards[:nr_items], range(nr_items))] # Apply factor to low rewards
+    rewards = low_list + rewards[nr_items:-nr_items] + high_list # Overwrite with the adjusted rewards list
+    mini_batch_ind = np.random.choice(list(range(len(rewards))), BATCH_SIZE, p = [x/sum(rewards) for x in rewards]) # Sample the indices of observations
+    # where the observations with high rewards are more likely sampled.
+    mini_batch = [replay_memory[ind] for ind in mini_batch_ind] # Retrieve the observations with the given indices 
+    current_states = np.array([transition[0] for transition in mini_batch]) # Extract the observations from the mini_batch
+    current_qs_list = model.predict(current_states) 
+    new_current_states = np.array([transition[3] for transition in mini_batch]) # Extract the new observation from the mini_batch
     future_qs_list = target_model.predict(new_current_states)
 
     X = []
     Y = []
     for index, (observation, action, reward, new_observation, done) in enumerate(mini_batch):
         if not done:
-            max_future_q = reward + discount_factor * np.max(future_qs_list[index]) # Future Q value
+            max_future_q = reward + DISCOUNT_RATE * np.max(future_qs_list[index]) # Future Q value
         else:
             max_future_q = reward
 
         current_qs = current_qs_list[index]
-        current_qs[action] = (1 - learning_rate) * current_qs[action] + learning_rate * max_future_q # Belman equation
+        current_qs[action] = (1 - LEARNING_RATE) * current_qs[action] + LEARNING_RATE * max_future_q # Belman equation
 
         X.append(observation)
         Y.append(current_qs)
         
-    model.fit(np.array(X), np.array(Y), batch_size=batch_size, verbose=0, shuffle=True) # Update neural network
+    model.fit(np.array(X), np.array(Y), batch_size=BATCH_SIZE, verbose=0, shuffle=True) # Update neural network
 
 
 def avail_item_values():
@@ -86,7 +97,7 @@ def avail_item_values():
     for item in range(env.N):
         if env.item_weights[item] + env.current_weight < env.max_weight:
             if env.item_limits[item] > 0:
-                avail_item_values.append(env.item_values[item] / env.item_weights[item])
+                avail_item_values.append(env.item_values[item])
             else:
                 avail_item_values.append(0)
         else:
@@ -95,11 +106,8 @@ def avail_item_values():
     return np.array(avail_item_values)
 
 def main():
+    
     epsilon = 1 # Epsilon-greedy algorithm in initialized at 1 meaning every step is random at the start
-    max_epsilon = 1 # You can't explore more than 100% of the time
-    min_epsilon = 0.01 # At a minimum, we'll always explore 1% of the time
-    decay = 0.0004 # How fast the epsilon decays
-
     # 1. Initialize the Target and Main models
     # Main Model (updated every 4 steps)
     model = agent()
@@ -108,11 +116,11 @@ def main():
     # Set the same weights as the Main Model for Target Model
     target_model.set_weights(model.get_weights())
     # Initialize Replay Memory
-    replay_memory = deque(maxlen=10_000)
+    replay_memory = deque(maxlen=800)
 
     steps_to_update_target_model = 0
         
-    for episode in range(train_episodes):
+    for episode in range(TRAIN_EPSISODES):
         total_training_rewards = 0
         observation = env.reset()
         done = False
@@ -125,7 +133,6 @@ def main():
             if random_number <= epsilon: # Explore
                 action = env.action_space.sample() 
             else: # Exploit
-                # Exploit best known action
                 predicted = model.predict(observation.reshape(1, env.N))
                 action = np.argmax(predicted)
                 
@@ -136,7 +143,6 @@ def main():
             # 3. Update the Main Network using the Bellman Equation
             if steps_to_update_target_model % 4 == 0 or done:
                 train(env, replay_memory, model, target_model, done)
-                #print("weights\n", model.get_weights())
 
             observation = new_observation
             total_training_rewards += reward
@@ -146,7 +152,7 @@ def main():
                 print(u'_________ n = {} _________'
                       u'\nTotal training rewards: {}\n'
                       u'Reward list = {}\n'
-                      u'item list {}\n'.format(episode,total_training_rewards,reward_list, item_list))
+                      u'item list = {}\n'.format(episode,total_training_rewards,reward_list, item_list))
 
                 if steps_to_update_target_model >= 100:
                     print('Copying main network weights to the target network weights')
@@ -154,7 +160,7 @@ def main():
                     steps_to_update_target_model = 0
                 break
 
-        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * episode)
+        epsilon = MIN_EPSILON + (MAX_EPSILON - MIN_EPSILON) * np.exp(-DECAY * episode)
         print("Epsilon:", epsilon, "\n")
     env.close()
 
